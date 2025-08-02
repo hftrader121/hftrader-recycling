d520af7e38105ebef8ff8940a817a1dc9f9a850d
hftrader121 <hftrader121@gmail.com>
2025-08-02 17:18:13 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/hftrader-recycling/', // 👈 Set this to your repo name
   plugins: [react()],
-})
+  base: "/hftrader-recycling/", // 👈 ADD THIS LINE
+});
